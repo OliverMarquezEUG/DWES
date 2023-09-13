122219900a954758eb116ffef7f7fa600a979f2b
OliverMarquezEUG <144775408+OliverMarquezEUG@users.noreply.github.com>
2023-09-13 14:03:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$meses = [
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo',
+    'Junio', 'Julio', 'Agosto', 'Septiembre',
+    'Octubre', 'Noviembre', 'Diciembre',
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Meses del año</title>
+</head>
+<body>
+    <h1>Meses del año</h1>
+    <ul>
+        <?php
+            foreach($meses as $mes){
+                echo '<li>'.$mes.'</li>';
+            }
+        ?>
+    </ul>
+</body>
+</html>
+
